b08b4ac8f4680229379b2f1c0860543d99ab72ac
khannnn <152291334+zeeuskhan@users.noreply.github.com>
2026-05-08 12:15:58 +0530

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Next.js SEO Optimizer</h1>
-      <p>Deployment successful.</p>
-    </main>
-  );
-}
